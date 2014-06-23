07f0d659933cc22c038da7d531f6d8fd841ae51c
Ash Darji <ashishdarji88@gmail.com>
2014-06-23 19:10:15 -0400

    added sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to products_url, :note => "Logged In!"
+      redirect_to products_url, :note => "Signed Up!"
     else
       render :new
     end
