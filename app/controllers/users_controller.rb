24059de5b36cd9c249809a26a7158a49209b07c1
Ash Darji <ashishdarji88@gmail.com>
2014-06-24 10:54:52 -0400

    added user names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:email, :password,:password_digest)
+    params.require(:user).permit(:name,:email, :password,:password_digest)
   end
 end
