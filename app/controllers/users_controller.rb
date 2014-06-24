85afc6f64f50d06ca5168fc529d11638f54c8717
Ash Darji <ashishdarji88@gmail.com>
2014-06-24 10:40:44 -0400

    finished functionality, needs styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to products_url, :notice => "Signed Up!"
     else
-      flash.now[:alert] = 'Couldn\'t sign up, try again and submit!'
       render :new
+      flash.now[:alert] = 'Couldn\'t sign up, try again and submit!'
     end
   end
-
   private
   def user_params
     params.require(:user).permit(:email, :password,:password_digest)
