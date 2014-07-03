144897003bb1d4f9c654b4c806027f66a74ab1f5
Ash Darji <ashishdarji88@gmail.com>
2014-07-03 16:28:06 -0400

    added saerch

--- Gemfile
+++ Gemfile
@@ -11,4 +11,3 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bcrypt-ruby', '~> 3.1.2'
-
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
   before_filter :ensure_logged_in, :only => [:show]
   def index
-    @products = Product.all
+    @products = if params[:search]
+      Product.where("name LIKE ?", "%#{params[:search]}%")
+    else
+      Product.all
+    end
   end
   def show
     @product = Product.find(params[:id])
