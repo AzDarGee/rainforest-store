1f4a6be87cdc4ff09ff7a2e40413f204078f26d5
Ash Darji <ashishdarji88@gmail.com>
2014-07-11 02:44:10 -0400

    adding loading indicator

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -25,4 +25,19 @@ $(document).ready(function() {
     });
   };
 
+  // Loading Indicator
+  $(function() {
+       $("#new_review")
+          .on('ajax:beforeSend', function() { // set this code to happen just before the ajax request is made
+            $("input[type='submit']") // make changes to the submit button
+              .val('Saving...') // change the text on the button
+              .attr('disabled', 'disabled'); // disable the button
+          })
+          .on('ajax:complete', function(data,status,xhr) {
+            $("input[type='submit']")
+              .val('Create Review')
+              .removeAttr('disabled');
+          });
+    });
+
 });
