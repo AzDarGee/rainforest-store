3ac5d60eb05f14c0e7b7ce4039b9680b6ab91bf8
Ash Darji <ashishdarji88@gmail.com>
2014-07-11 06:32:45 -0400

    more styling

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,6 +3,7 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function() {
+
   // AJAX Search
   $("#search-form").submit(function(event) {
     var searchValue = $("#search").val();;
